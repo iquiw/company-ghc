4d1f1e3c9529b1a833fa58e835226cebf0e415b7
Iku Iwasa <iku.iwasa@gmail.com>
2016-03-15 23:10:49 +0900

    Bump version to v1.2.2

--- company-ghc.el
+++ company-ghc.el
@@ -4,7 +4,7 @@
 
 ;; Author:    Iku Iwasa <iku.iwasa@gmail.com>
 ;; URL:       https://github.com/iquiw/company-ghc
-;; Version:   1.2.1
+;; Version:   1.2.2
 ;; Package-Requires: ((cl-lib "0.5") (company "0.8.0") (ghc "5.4.0.0") (emacs "24"))
 ;; Keywords:  haskell, completion
 ;; Stability: experimental
